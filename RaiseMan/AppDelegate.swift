2a0190f5225e01017bcb186b0af93be075929586
Rubén Jiménez <ruben@21ninjas.es>
2015-11-18 23:17:37 +0000

    Initial Commit

---
+++ RaiseMan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RaiseMan
+//
+//  Created by Rubén Jiménez on 18/11/15.
+//  Copyright © 2015 21ninjas, SL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
