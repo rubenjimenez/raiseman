ea039354ef56efed7560e2094be0ad37f361e5d4
Rubén Jiménez <ruben@21ninjas.es>
2015-11-21 19:21:15 +0000

    Add sorting to columns

---
+++ RaiseMan/Employee.swift
@@ -0,0 +1,15 @@
+//
+//  Employee.swift
+//  RaiseMan
+//
+//  Created by Rubén Jiménez on 18/11/15.
+//  Copyright © 2015 21ninjas, SL. All rights reserved.
+//
+
+import Foundation
+
+class Employee : NSObject {
+    
+    var name: String? = "New Employee"
+    var raise: Float = 0.05
+}
