0b3a49ae159f1211464d8e13ca2d1d8e5a66109d
Rubén Jiménez <ruben@21ninjas.es>
2015-12-18 00:20:58 +0000

    Add validation for 0.0 values in raise textfield

--- RaiseMan/Employee.swift
+++ RaiseMan/Employee.swift
@@ -12,4 +12,22 @@ class Employee : NSObject {
     
     var name: String? = "New Employee"
     var raise: Float = 0.05
+    
+    func validateRaise(raiseNumberPointer: AutoreleasingUnsafeMutablePointer<NSNumber?>, error outError: NSErrorPointer) -> Bool {
+        
+        let raiseNumber = raiseNumberPointer.memory
+        
+        if raiseNumber == 0.0 {
+            let domain = "UserInputValidationErrorDomain"
+            let code = 0
+            let userInfo = [NSLocalizedDescriptionKey: "An employee's raise must be a number."]
+            
+            outError.memory = NSError(domain: domain, code: code, userInfo: userInfo)
+            
+            return false
+        }
+        else {
+            return true
+        }
+    }
 }
